a38522fbf65ba2d9f4b4da4776e0380ddfd96408
Keian Mar <150799485+7kei@users.noreply.github.com>
2025-01-31 13:42:57 +0800

    Add portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,121 @@
+const perfectFrameTime = 1000 / 60;
+var canvas;
+var context;
+let deltaTime = 0;
+let lastTimestamp = 0;
+
+var CANVAS_WIDTH;
+var CANVAS_HEIGHT;
+
+let arrToDraw = [];
+
+class Miku {
+    constructor(context, x, y) {
+        this.x = (typeof x === undefined) ? 0 : x;
+        this.y = (typeof y === undefined) ? -100 : y;
+        this.context = context;
+        this.sprite = new Image();
+        this.sprite.src = 'images/fallingmiku.png';
+        this.toWalk = undefined;
+        this.wait = 0;
+
+        this.isStanding = false;
+
+    }
+
+    update() {
+        this.context.drawImage(this.sprite, this.x, this.y);
+
+        if (this.wait > 0) {
+            this.sprite.src = 'images/standingmiku.png';
+            this.wait--;
+            if (this.y+100 > CANVAS_HEIGHT) this.y = CANVAS_HEIGHT-100;
+            return;
+        }
+
+        if (this.y+100 < CANVAS_HEIGHT) {
+            this.isFalling = true;
+        }
+
+        if (this.y+100 > CANVAS_HEIGHT && this.isFalling) {
+            this.isStanding = true;
+        }
+
+        if (this.isStanding) {
+            this.isFalling = false;
+            this.sprite.src = 'images/standingmiku.png';
+            
+        }
+
+        if (this.toWalk != 0) {
+            if (this.y+100 > CANVAS_HEIGHT) this.y = CANVAS_HEIGHT-100;
+            if (this.toWalk > this.x) {
+                this.sprite.src = 'images/walkingmikuR.png';
+                if (this.x-1 == this.toWalk) {
+                    this.isStanding = true;
+                    this.wait = 200;
+                }
+
+                this.x++;
+            }
+            if (this.toWalk < this.x) {
+                this.sprite.src = 'images/walkingmikuL.png';
+                if (this.x-1 == this.toWalk) {
+                    this.isStanding = true;
+                    this.wait = 200;
+                }
+
+                this.x--;
+            }
+
+            if (this.toWalk == this.x || (this.toWalk === undefined && !this.isFalling) ) {
+                this.toWalk = Math.floor(Math.random() * ((CANVAS_WIDTH-60) - 0) ) + 0;
+                this.isStanding = false;
+            }
+
+        }
+
+        if (this.isFalling) {
+            this.sprite.src = 'images/fallingmiku.png';
+            this.y += 5;
+            this.isStanding = false;
+        }
+
+        if (this.y > CANVAS_HEIGHT) {
+            arrToDraw.splice(arrToDraw.indexOf(this),1);
+        }
+    }
+}
+
+function update(timestamp) {
+    requestAnimationFrame(update);
+    deltaTime = (timestamp - lastTimestamp) / perfectFrameTime;
+    lastTimestamp = timestamp;
+
+    context.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    arrToDraw.forEach(element => {
+        element.update();
+    });
+}
+
+function start() {
+    requestAnimationFrame(update);
+}
+
+window.addEventListener("click", (event) => {
+    var newMiku = new Miku(context, event.x, event.y);
+    arrToDraw.push(newMiku);
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    canvas = document.getElementById("miku-canvas");
+    context = canvas.getContext("2d");
+
+    CANVAS_WIDTH = canvas.width = window.innerWidth;
+    CANVAS_HEIGHT = canvas.height = window.innerHeight;
+
+    const miku1 = new Miku(context);
+    arrToDraw.push(miku1);
+
+    start();
+});
